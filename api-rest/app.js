08ea21a4b387a9fb7dfd2499e586b40cc9b86e24
Hector Camacho <danielcatu@gmail.com>
2018-10-12 20:09:39 -0500

    Acabe el primer inciso

--- api-rest/app.js
+++ api-rest/app.js
@@ -14,8 +14,13 @@ app.get('/api/xlsx2j', (req, res) => {
   func.xlsx2j(res)
 })
 
-app.get('/api/HOTEL_NAME/:name-:state', (req, res) => {
-  func.Hotel.find({ name: { $regex: req.params.name, $options: 'i' }, state: { $regex: req.params.state, $options: 'i' } }, (err, hotels) => {
+app.get('/api/HOTEL_NAME/:name-:state-:type-:size', (req, res) => {
+  func.Hotel.find({
+    name: { $regex: req.params.name, $options: 'i' },
+    state: { $regex: req.params.state, $options: 'i' },
+    type: { $regex: req.params.type, $options: 'i' },
+    size: { $regex: req.params.size, $options: 'i' }
+  }, (err, hotels) => {
     if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
     res.status(200).send({ hotels })
   })
