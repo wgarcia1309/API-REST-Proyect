a58dae4efd811e5ac73f755b3a93e0fc64f20993
Mkultr730 <memolinares@uninorte.edu.co>
2018-10-13 00:04:55 -0500

    Creación de usuario en progreso

---
+++ api-rest/controllers/user.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const mongoose = require('mogoose')
+const User = require('../models/Schema').user
+const service = require('../service')
+
+function signUp (req, res) {
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+  })
+
+  user.save((err) => {
+    if(err) res.status(500).send({ message: 'Error al crear el usuario: ${err}'})
+
+    return res.status(200).send({ token: service.createToken(user)})
+  })
+}
+
+function signIn (req, res) {
+  User.find({ email: req.body.email}, (err, user) => {
+    if(err) return res.status(500).send({ message: err })
+    if(!user) return res.status(404).send({ message: 'No existe el usuario' })
+
+    req.user = user
+    res.status(200).send({
+      message: 'Te has logueado correctamente',
+      token: service.createToken(user)
+    })
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn
+}
--- api-rest/models/Schema.js
+++ api-rest/models/Schema.js
@@ -1,6 +1,8 @@
 'use strict'
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
+const crypto = require('crypto')
 
 const HotelSchema = Schema({
   name: { type: String, required: true },
@@ -22,8 +24,35 @@ const UserSchema = Schema({
   password: { type: String, select: false },
   name: String,
   lastname: String,
-  address: String
+  address: String,
+  avatar: String
 })
+
+UserSchema.pre('save', (next)=> {
+  let user = this
+  if(!user.isModified('password')) return next()
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) return next(err)
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if(err) return next(err)
+
+      user.password = hash
+      next()
+    })
+  })
+})
+
+UserSchema.methods.gravatar = function () {
+  if(!this.email) return 'https://gravatar.com/avatar/?s=200&d=retro'
+
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex')
+  return 'https://gravatar.com/avatar/${md5}?s=200&d=retro'
+}
+
+
+
 let Hotel = mongoose.model('Hotel', HotelSchema)
 let User = mongoose.model('User', UserSchema)
 module.exports = { Hotel, User }
