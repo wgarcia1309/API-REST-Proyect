727dae06f3af17cc34b4b42653fc2241ded3622c
Willian Garcia <wbovea@uninorte.edu.co>
2018-10-14 20:04:13 -0500

    Se modificaron los metodos(GET,POST,DELETE)

--- api-rest/app.js
+++ api-rest/app.js
@@ -15,13 +15,13 @@ app.get('/api/xlsx2j', (req, res) => {
 })
 
 // creacion de un nuevo usuario
-app.get('/api/new_user/:email-:password-:name-:lastname-:address', (req, res) => {
+app.post('/api/new_user/', (req, res) => {
   let user = new func.User()
-  user.email = req.params.email
-  user.password = req.params.password
-  user.name = req.params.name
-  user.lastname = req.params.lastname
-  user.address = req.params.address
+  user.email = req.query.email
+  user.password = req.query.password
+  user.name = req.query.name
+  user.lastname = req.query.lastname
+  user.address = req.query.address
 
   user.save((err, User) => {
     if (err) return res.status(500).send({ message: `Error al salvar la base de datos ${err}` })
@@ -30,10 +30,10 @@ app.get('/api/new_user/:email-:password-:name-:lastname-:address', (req, res) =>
 })
 
 // actualizacion de un usuario
-app.get('/api/update_user/:id-:newpassword-:newadd', (req, res) => {
-  func.User.update({ _id: req.params.id }, {
-    password: req.params.newpassword,
-    address: req.params.newadd
+app.post('/api/update_user/', (req, res) => {
+  func.User.update({ _id: req.query.id }, {
+    password: req.query.newpassword,
+    address: req.query.newadd
   }, (err, users) => {
     if (err) return res.status(500).send({ value: 0, message: `Error al realizar la peticion ${err}` })
     if (users.n === 0) return res.status(404).send({ value: 0, message: `File no found` })
@@ -44,16 +44,16 @@ app.get('/api/update_user/:id-:newpassword-:newadd', (req, res) => {
 // Hacer una reservación
 app.get('/api/reserve/:hotel_id-:user_id-:sdate-:fdate', (req, res) => {
   func.Hotel.find({
-    _id: req.params.hotel_id
+    _id: req.query.hotel_id
   }, (err, hotels) => {
     if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
     else if (hotels.length === 0) return res.status(500).send({ message: `No existe un hotel con ese Id` })
     else {
       let reserve = new func.Reserve()
-      reserve.hotelId = req.params.hotel_id
-      reserve.userId = req.params.user_id
-      reserve.sDate = req.params.sdate
-      reserve.fDate = req.params.fdate
+      reserve.hotelId = req.query.hotel_id
+      reserve.userId = req.query.user_id
+      reserve.sDate = req.query.sdate
+      reserve.fDate = req.query.fdate
       reserve.save((err, Reserve) => {
         if (err) return res.status(500).send({ message: `Error al salvar la base de datos ${err}` })
         else res.status(200).send({ _id: Reserve['_id'] })
@@ -63,12 +63,12 @@ app.get('/api/reserve/:hotel_id-:user_id-:sdate-:fdate', (req, res) => {
 })
 
 // busqueda de hotel por nombre,estado y tipo
-app.get('/api/HOTEL_NAME/:name-:state-:type-:size', (req, res) => {
+app.get('/api/HOTEL_NAME/', (req, res) => {
   func.Hotel.find({
-    name: { $regex: req.params.name, $options: 'i' },
-    state: { $regex: req.params.state, $options: 'i' },
-    type: { $regex: req.params.type, $options: 'i' },
-    size: { $regex: req.params.size, $options: 'i' }
+    name: { $regex: req.query.name, $options: 'i' },
+    state: { $regex: req.query.state, $options: 'i' },
+    type: { $regex: req.query.type, $options: 'i' },
+    size: { $regex: req.query.size, $options: 'i' }
   }, (err, hotels) => {
     if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
     res.status(200).send({ hotels })
@@ -76,20 +76,20 @@ app.get('/api/HOTEL_NAME/:name-:state-:type-:size', (req, res) => {
 })
 
 // creacion de un hotel
-app.get('/api/new_hotel/:nam-:add-:lat-:lon-:state-:pho-:fax-:ema-:web-:typ-:room-:siz', (req, res) => {
+app.post('/api/new_hotel/', (req, res) => {
   let hotel = new func.Hotel()
-  hotel.name = req.params.nam
-  hotel.address = req.params.add
-  hotel.latitude = req.params.lat
-  hotel.longitude = req.params.lon
-  hotel.state = req.params.state
-  hotel.phone = req.params.pho
-  hotel.fax = req.params.fax
-  hotel.email = req.params.ema
-  hotel.website = req.params.web
-  hotel.type = req.params.typ
-  hotel.room = req.params.room
-  hotel.size = req.params.siz
+  hotel.name = req.query.nam
+  hotel.address = req.query.add
+  hotel.latitude = req.query.lat
+  hotel.longitude = req.query.lon
+  hotel.state = req.query.state
+  hotel.phone = req.query.pho
+  hotel.fax = req.query.fax
+  hotel.email = req.query.ema
+  hotel.website = req.query.web
+  hotel.type = req.query.typ
+  hotel.room = req.query.room
+  hotel.size = req.query.siz
   hotel.save((err, Hotel) => {
     if (err) return res.status(500).send({ message: `Error al salvar la base de datos ${err}` })
     res.status(200).send({ _id: Hotel['_id'] })
@@ -97,14 +97,14 @@ app.get('/api/new_hotel/:nam-:add-:lat-:lon-:state-:pho-:fax-:ema-:web-:typ-:roo
 })
 
 // Actualizacion de un hotel
-app.get('/api/update_hotel/:id-:htype-:rooms-:phone-:web-:email', (req, res) => {
-  let id = req.params.id
+app.post('/api/update_hotel/', (req, res) => {
+  let id = req.query.id
   func.Hotel.update({ _id: id }, {
-    phone: req.params.phone,
-    email: req.params.email,
-    website: req.params.web,
-    type: req.params.htype,
-    room: req.params.rooms
+    phone: req.query.phone,
+    email: req.query.email,
+    website: req.query.web,
+    type: req.query.htype,
+    room: req.query.rooms
   }, (err, hotels) => {
     if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
     else if (hotels.n === 0) return res.status(404).send({ message: 'El ID del hotel no existe' })
@@ -113,8 +113,8 @@ app.get('/api/update_hotel/:id-:htype-:rooms-:phone-:web-:email', (req, res) => 
 })
 
 // Eliminacion de hotel
-app.get('/api/DEL_HOTEL/:id', (req, res) => {
-  let id = req.params.id
+app.delete('/api/DEL_HOTEL/', (req, res) => {
+  let id = req.query.id
   func.Hotel.deleteOne({
     _id: id
   }, (err, hotels) => {
